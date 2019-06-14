4bd8bc5fa8c95d51a6252f1f9f32378e3b2bae92
BoringSingleDog <www.1715731900@qq.com>
2019-06-14 12:08:11 +0800

    文件修改

--- MITSDatabaseBackUp.cpp
+++ MITSDatabaseBackUp.cpp
@@ -29,6 +29,7 @@ MITSDatabaseBackUp::MITSDatabaseBackUp(QWidget *parent)
 	connect(ui.pb_Recover,SIGNAL(clicked()),this,SLOT(doRecover()));
 	connect(downLoad,SIGNAL(triggered()),this,SLOT(doDealAction()));
 	connect(deleteList,SIGNAL(triggered()),this,SLOT(doDealAction()));
+	
 }
 
 MITSDatabaseBackUp::~MITSDatabaseBackUp()
